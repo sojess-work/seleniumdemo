7720aa538b374eee846f839e485c626aae5d4a04
sojess-work <92088687+sojess-work@users.noreply.github.com>
2021-12-09 23:18:10 +0530

    Add files via upload

---
+++ selenium2/src/test/java/selenium2/NewTest1.java
@@ -0,0 +1,42 @@
+package selenium2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest1 {
+	WebDriver driver;
+  @Test
+  public void test_search() throws InterruptedException {
+	  	WebElement search = driver.findElement(By.id("search"));
+		search.sendKeys("Astrid TV Unit");
+		driver.findElement(By.id("search_button")).click();
+		System.out.println("Searched Astrid TV Unit");
+		Thread.sleep(5000);
+  }
+  @BeforeTest
+  public void beforeTest() throws InterruptedException {
+	  String projectpath =System.getProperty("user.dir");
+	  System.setProperty("webdriver.chrome.driver",projectpath+"/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		String baseUrl = "https://www.urbanladder.com/tv-units?src=g_topnav_storage_living-storage_tv-units";
+		driver.get(baseUrl);
+		TimeUnit.SECONDS.sleep(5);
+		driver.findElement(By.linkText("Close")).click();
+		System.out.println("Closed pop-up");
+		TimeUnit.SECONDS.sleep(5);
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.close();
+  }
+
+}
